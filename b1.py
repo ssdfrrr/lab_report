2242d7c85a03c03f445c3d4d854fd34d4e98d8dd
ssdfrrr <tanchik.minigunchik@gmail.com>
2025-04-11 04:33:10 +0300

    Create b1.py

---
+++ b1.py
@@ -0,0 +1,14 @@
+import random
+number = random.randint(0, 100)
+ans = None
+while number != ans:
+    try:
+        ans = int(input("Число: "))
+        if number < ans:
+            print ("меньше")
+        elif number > ans:
+            print ("больше")
+        else:
+            print("вы угадали")
+    except ValueError:
+        print("введите целое")
